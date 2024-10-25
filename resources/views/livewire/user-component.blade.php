4651f084a0be7ed151bef1af3d3884926fc6d589
CristianBarrios983 <barrioscristian.9908@gmail.com>
2024-10-25 20:33:40 -0300

    Vista de usuarios

--- resources/views/livewire/user-component.blade.php
+++ resources/views/livewire/user-component.blade.php
@@ -1,3 +1,72 @@
-<div>
-    Usuarios
-</div>
+<div class="content p-4">
+            <h1 class="text-dark mb-4">Usuarios</h1>
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <button class="btn btn-primary">Registrar</button>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover" id="myTable">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Apellido</th>
+                            <th scope="col">Usuario</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Rol</th>
+                            <th scope="col" class="text-center">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Cristian</td>
+                            <td>Barrios</td>
+                            <td>admin</td>
+                            <td>cristian@gmail.com</td>
+                            <td class="text-success">Administrador</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Lucia</td>
+                            <td>Duarte</td>
+                            <td>lucia</td>
+                            <td>luciaduarte@gmail.com</td>
+                            <td class="text-success">Recepcionista</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Sara</td>
+                            <td>Rodriguez</td>
+                            <td>sara</td>
+                            <td>sararodriguez@gmail.com</td>
+                            <td class="text-success">De Limpieza</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <!-- Más filas -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
