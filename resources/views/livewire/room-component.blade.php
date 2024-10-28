e6f26d9426bed05cadb49422d1c0522e8de3c878
CristianBarrios983 <barrioscristian.9908@gmail.com>
2024-10-28 11:43:39 -0300

    Vista habitaciones

--- resources/views/livewire/room-component.blade.php
+++ resources/views/livewire/room-component.blade.php
@@ -1,3 +1,108 @@
-<div>
-    Habitaciones
-</div>
+<div class="content p-4">
+            <h1 class="text-dark mb-4">Habitaciones</h1>
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <button class="btn btn-primary">Registrar</button>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover" id="myTable">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Número de Habitación</th>
+                            <th scope="col">Capacidad</th>
+                            <th scope="col">Tamaño (m²)</th>
+                            <th scope="col">Vistas</th>
+                            <th scope="col">Tipo de Cama</th>
+                            <th scope="col">Precio por Noche</th>
+                            <th scope="col">Disponibilidad</th>
+                            <th scope="col">Tipo de Habitación</th>
+                            <th scope="col">Piso</th>
+                            <th scope="col" class="text-center">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>101</td>
+                            <td>2</td>
+                            <td>25</td>
+                            <td>Mar</td>
+                            <td>King Size</td>
+                            <td>$120</td>
+                            <td class="text-success">Disponible</td>
+                            <td>Suite</td>
+                            <td>1</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>102</td>
+                            <td>4</td>
+                            <td>30</td>
+                            <td>Jardín</td>
+                            <td>Queen Size</td>
+                            <td>$150</td>
+                            <td class="text-danger">Ocupada</td>
+                            <td>Deluxe</td>
+                            <td>1</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>203</td>
+                            <td>3</td>
+                            <td>28</td>
+                            <td>Ciudad</td>
+                            <td>2 Twin</td>
+                            <td>$90</td>
+                            <td class="text-success">Disponible</td>
+                            <td>Estándar</td>
+                            <td>2</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>305</td>
+                            <td>1</td>
+                            <td>18</td>
+                            <td>Patio</td>
+                            <td>Single</td>
+                            <td>$70</td>
+                            <td class="text-danger">Ocupada</td>
+                            <td>Individual</td>
+                            <td>3</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <!-- Más filas de muestra -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
