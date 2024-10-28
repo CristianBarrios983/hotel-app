c05b2fe944e194e57416e3d8ee148a6a9cf2b093
CristianBarrios983 <barrioscristian.9908@gmail.com>
2024-10-28 12:16:32 -0300

    Creando componente de tipo de habitacion

---
+++ app/Livewire/TipoHabitacionComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TipoHabitacionComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.tipo-habitacion-component');
+    }
+}
