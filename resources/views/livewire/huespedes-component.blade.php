c5653254e5b3a89081412f446f9292022200c182
YesiM024 <yesimosqueda1234567@gmail.com>
2024-11-01 13:47:28 -0300

    vista de huespedes

--- resources/views/livewire/huespedes-component.blade.php
+++ resources/views/livewire/huespedes-component.blade.php
@@ -1,3 +1,118 @@
 <div>
-    Huespedes
+<div class="content p-4">
+            <h1 class="text-dark mb-4">Huespedes</h1>
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <button class="btn btn-primary">Registrar</button>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover" id="myTable">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Apellido</th>
+                            <th scope="col">Fecha de nacimiento</th>
+                            <th scope="col">Nacionalidad</th>
+                            <th scope="col">tipo de documento</th>
+                            <th scope="col">Nro de documento</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Telefono</th>
+                            <th scope="col">Fecha creacion</th>
+                            <th scope="col">Fecha actualizacion</th>
+                            <th scope="col" class="text-center">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Maria</td>
+                            <td>Gimenez</td>
+                            <td>25/08/1992</td>
+                            <td>Argentina</td>
+                            <td>Dni</td>
+                            <td>36.899.032</td>
+                            <td>Marijogim@gmail.com</td>
+                            <td>3704030345</td>
+                            <td>15/10/2024</td>
+                            <td>17/10/2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Rodrigo</td>
+                            <td>Diaz</td>
+                            <td>24/03/1989</td>
+                            <td>Argentino</td>
+                            <td>Dni</td>
+                            <td>33.855.921</td>
+                            <td>Rodri022diaz@gmail.com</td>
+                            <td>3715963541</td>
+                            <td>05/11/2024</td>
+                            <td>07/11/2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Sandra</td>
+                            <td>Paredes</td>
+                            <td>28/05/1988</td>
+                            <td>Argentino</td>
+                            <td>Dni</td>
+                            <td>30.258.323</td>
+                            <td>Sandritap@gmail.com</td>
+                            <td>3883597461</td>
+                            <td>2/10/2024</td>
+                            <td>3/10/2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>Lorenzo</td>
+                            <td>Peaz</td>
+                            <td>18/02/1997</td>
+                            <td>Argentino</td>
+                            <td>Dni</td>
+                            <td>40.288.644</td>
+                            <td>Lolo84@gmail.com</td>
+                            <td>3704321654</td>
+                            <td>16/11/2024</td>
+                            <td>18/11/2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <!-- Más filas de muestra -->
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+        
+    </div>
 </div>
