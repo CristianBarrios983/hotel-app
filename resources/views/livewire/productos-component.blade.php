cae194bc8a8ca0278e28791755b4349f9a554d0f
YesiM024 <yesimosqueda1234567@gmail.com>
2024-11-03 12:25:15 -0300

    vista de productos

--- resources/views/livewire/productos-component.blade.php
+++ resources/views/livewire/productos-component.blade.php
@@ -1,3 +1,104 @@
 <div>
-    Productos
+<div class="content p-4">
+            <h1 class="text-dark mb-4">Productos</h1>
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <button class="btn btn-primary">Registrar</button>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover" id="myTable">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Descripcion</th>
+                            <th scope="col">Precio</th>
+                            <th scope="col">Stock minimo</th>
+                            <th scope="col">Categoria</th>
+                            <th scope="col">Fecha de creacion</th>
+                            <th scope="col">Fecha de modificacion</th>
+                            <th scope="col" class="text-center"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Papel Higiénico</td>
+                        <td>Rollo de papel higiénico de doble hoja.</td>
+                        <td>$1.50</td>
+                        <td>40</td>
+                        <td>Productos de Higiene</td>
+                        <td>18-01-2020</td>
+                        <td>20-10-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Gel de Ducha</td>
+                        <td>Gel refrescante con aroma a eucalipto.</td>
+                        <td>$6.00</td>
+                        <td>20</td>
+                        <td>Productos de Higiene</td>
+                        <td>04-03-2021</td> 
+                        <td>08-09-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Champú de Lujo</td>
+                        <td>Champú con aroma premium.</td>
+                        <td>$12.00</td>
+                        <td>15</td>
+                        <td>Productos de Higiene</td>
+                        <td>07-01-2020</td>
+                        <td>09-08-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                        <th scope="row">4</th>
+                        <td>Toalla de Mano</td>
+                        <td>Toalla de mano de algodón, fácil de secar.</td>
+                        <td>$5.00</td>
+                        <td>15</td>
+                        <td>Ropa de Baño</td>
+                        <td>20-04-2020</td>
+                        <td>25-07-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+        
+    </div>
+
+</div>
 </div>
