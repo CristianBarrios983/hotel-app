ef197c7e1b5927c193b75d529b084102c6e266f6
YesiM024 <yesimosqueda1234567@gmail.com>
2024-11-03 15:41:31 -0300

    vista de servivios

--- resources/views/livewire/servicios-component.blade.php
+++ resources/views/livewire/servicios-component.blade.php
@@ -1,3 +1,93 @@
 <div>
-    Servicios
+<div class="content p-4">
+            <h1 class="text-dark mb-4">Servicios</h1>
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <button class="btn btn-primary">Registrar</button>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover" id="myTable">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Servicio usado</th>
+                            <th scope="col">Reserva estancia</th>
+                            <th scope="col">Servicio adicional</th>
+                            <th scope="col">cantidad</th>
+                            <th scope="col">Fecha de uso</th>
+                            <th scope="col" class="text-center">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Spa</td>
+                            <td>RES1002</td>
+                            <td>Masaje de relajación</td>
+                            <td>1</td>
+                            <td>11-02-2021</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Gimnasio</td>
+                            <td>RES1003</td>
+                            <td>Acceso diario</td>
+                            <td>3</td>
+                            <td>11-03-2022</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Lavandería</td>
+                            <td>RES1004</td>
+                            <td>Lavado de ropa</td>
+                            <td>5</td>
+                            <td>20-04-2023</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>Piscina</td>
+                            <td>RES1007</td>
+                            <td>Acceso diario</td>
+                            <td>1</td>
+                            <td>26-02-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+        
+    </div>
+
 </div>
