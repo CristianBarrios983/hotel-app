ed20d5a2c14238af4859495c92407ef82372703e
YesiM024 <yesimosqueda1234567@gmail.com>
2024-11-03 11:45:28 -0300

    vista de proveedore

--- resources/views/livewire/proveedores-component.blade.php
+++ resources/views/livewire/proveedores-component.blade.php
@@ -1,3 +1,108 @@
 <div>
-    Proveedores
+<div class="content p-4">
+            <h1 class="text-dark mb-4">Proveedores</h1>
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <button class="btn btn-primary">Registrar</button>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover" id="myTable">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nombre de Proveedor</th>
+                            <th scope="col">Telefono</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Direccion</th>
+                            <th scope="col">Descripcion</th>
+                            <th scope="col">Categoria</th>
+                            <th scope="col">Fecha de creacion</th>
+                            <th scope="col">Fecha de actualizacion</th>
+                            <th scope="col" class="text-center"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Servicios Globales S.A.</td>
+                        <td>+54 11 4321-5678</td>
+                        <td>contacto@serviciosglobales.com</td>
+                        <td>Av. Libertador 1000, Buenos Aires</td>
+                        <td>Proveedor de servicios de limpieza y mantenimiento.</td>
+                        <td>Servicios</td>
+                        <td>15-05-2020</td>
+                        <td>20-10-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Distribuidora Alimentos SRL</td>
+                            <td>+54 11 8765-4321</td>
+                            <td>info@distribalimentos.com</td>
+                            <td>Calle 25 de Mayo 450, Córdoba</td>
+                            <td>Distribuidor de alimentos para el sector hotelero.</td>
+                            <td>Alimentación</td>
+                            <td>06-02-2020</td>
+                            <td>09-11-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>ElectroTech Soluciones</td>
+                            <td>+54 291 3456-7890</td>
+                            <td>ventas@electrotech.com</td>
+                            <td>San Martín 700, Mendoza</td>
+                            <td>Proveedor de equipos electrónicos y mantenimiento técnico.</td>
+                            <td>Productos</td>
+                            <td>11-03-2020</td>
+                            <td>13-06-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">4</th>
+                            <td>EcoGreen S.A.</td>
+                            <td>+54 341 2234-5566</td>
+                            <td>soporte@ecogreen.com</td>
+                            <td>Mitre 2200, Rosario</td>
+                            <td>Productos ecológicos para limpieza y mantenimiento.</td>
+                            <td>Limpieza</td>
+                            <td>07-02-2020</td>
+                            <td>11-10-2024</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+        
+    </div>
+
 </div>
