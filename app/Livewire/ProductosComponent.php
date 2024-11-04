aee9f4b5b6183b5faaf2b8b7367367c61f614c6c
CristianBarrios983 <barrioscristian.9908@gmail.com>
2024-11-04 20:14:10 -0300

    Creacion del modelo y migracion de Productos

--- app/Livewire/ProductosComponent.php
+++ app/Livewire/ProductosComponent.php
@@ -3,11 +3,16 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Productos;
 
 class ProductosComponent extends Component
 {
+
+    public $productos, $productoId, $nombreProducto, $descripcion, $precio, $stock, $stockMinimo, $categoria;
+
     public function render()
     {
-        return view('livewire.productos-component');
+        $this->productos = Productos::all();
+        return view('livewire.productos-component')->layout('layouts.app');
     }
 }
--- app/Models/Productos.php
+++ app/Models/Productos.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Productos extends Model
 {
     use HasFactory;
+
+    protected $table = 'productos';
+
+    protected $fillable = ['nombre_producto', 'descripcion', 'precio', 'stock', 'stock_minimo', 'categoria_id'];
 }
