1d5b499cef4fde2b9b86d3a284fcd153f0338f06
YesiM024 <yesimosqueda1234567@gmail.com>
2024-11-07 13:29:28 -0300

    vista de pedidos

--- resources/views/livewire/pedidos-component.blade.php
+++ resources/views/livewire/pedidos-component.blade.php
@@ -1,3 +1,44 @@
-<div>
-    Pedidos
-</div>
+<div class="content p-4">
+            <h1 class="text-dark mb-4">Pedidos</h1>
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <button class="btn btn-primary">Registrar</button>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover" id="myTable">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Pedido</th>
+                            <th scope="col">Proveedor</th>
+                            <th scope="col">Fecha de pedido</th>
+                            <th scope="col">Entrega de pedido</th>
+                            <th scope="col">Estado</th>
+                            <th scope="col">Total de pedido</th>
+                            <th scope="col" class="text-center">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>12</td>
+                            <td>Proveedor de limpeza SRL</td>
+                            <td>11-08-2024</td>
+                            <td>15-08-2024</td>
+                            <td>Pendiente</td>
+                            <td>$70000</td>
+                            <td class="text-center">
+                                <a href="#" class="btn btn-warning btn-sm" title="Editar">
+                                    <i class="bi bi-pencil-fill"></i>
+                                </a>
+                                <a href="#" class="btn btn-danger btn-sm" title="Eliminar">
+                                    <i class="bi bi-trash-fill"></i>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        
+        
+    </div>
