fd798be18c9aaa92f8ec54a6eb13a3839aeab4f0
CristianBarrios983 <barrioscristian.9908@gmail.com>
2024-11-26 21:08:51 -0300

    Añadi DarkMode temporalmente

--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -79,6 +79,13 @@
           <a class="nav-link" href="/configuracion">Configuracion</a>
         </li>
 
+        <li class="nav-item">
+                    <div class="form-check form-switch d-flex justify-content-center align-items-center">
+                      <input class="form-check-input me-1 text-white" type="checkbox" role="switch" id="darkModeToggle">
+                      <label class="form-check-label nav-link disabled align-items-center" for="flexSwitchCheckDefault"><i class="fa-solid fa-moon" id="icon"></i></label>
+          </div>
+        </li>
+
         <!-- Settings Dropdown -->
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle text-info" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name">
