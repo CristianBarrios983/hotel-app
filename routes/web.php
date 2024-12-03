0cdecacce708840714dc95a22541f9a93482f361
CristianBarrios983 <barrioscristian.9908@gmail.com>
2024-12-03 16:44:24 -0300

    Creacion del componente para Reportes

--- routes/web.php
+++ routes/web.php
@@ -97,4 +97,8 @@ Route::get('/facturacion/{reserva}', function($reserva) {
 ->name('facturacion')
 ->middleware(['auth']);
 
+Route::get('/reportes', function () {
+    return view('reportes'); 
+})->name('reportes')->middleware(['auth']);
+
 require __DIR__.'/auth.php';
